65d1960765354308d718178df1b89ca74275a722
jonas.lindgren <jonas.lindgren@774f44fe-ff29-0410-bf63-e573c7ae51ad>
2007-02-21 17:52:04 +0000

    Added items remotely
    H:\My Documents\Visual Studio Projects\Bomberman\Bomberman.ncb
    H:\My Documents\Visual Studio Projects\Bomberman\Bomberman.sln
    H:\My Documents\Visual Studio Projects\Bomberman\Bomberman.suo
    H:\My Documents\Visual Studio Projects\Bomberman\Bomberman.vcproj
    H:\My Documents\Visual Studio Projects\Bomberman\cube.cpp
    H:\My Documents\Visual Studio Projects\Bomberman\cube.h
    H:\My Documents\Visual Studio Projects\Bomberman\model.cpp
    H:\My Documents\Visual Studio Projects\Bomberman\model.h
    H:\My Documents\Visual Studio Projects\Bomberman\Object.cpp
    H:\My Documents\Visual Studio Projects\Bomberman\Object.h
    H:\My Documents\Visual Studio Projects\Bomberman\test.cpp
    H:\My Documents\Visual Studio Projects\Bomberman\bomberman.cpp

---
+++ Bomberman 1.0/model.cpp
@@ -0,0 +1,72 @@
+#include "model.h"
+#include "freeglut.h"
+
+
+Model::Model() {
+	return;
+}
+
+
+void Model::draw() {
+	const static GLfloat cubeVertexes[][3] = {
+		{-0.5, 0.5, -0.5},
+		{0.5, 0.5, -0.5},
+		{-0.5, 0.5, 0.5},
+		{0.5, 0.5, 0.5},
+		{-0.5, -0.5, -0.5},
+		{0.5, -0.5, -0.5},
+		{-0.5, -0.5, 0.5},
+		{0.5, -0.5, 0.5}
+	};
+
+	glBegin(GL_POLYGON);
+	glColor3f(1.0, 0.0, 1.0);
+	glNormal3f(0, 1, 0);
+	glVertex3fv(cubeVertexes[0]);
+	glVertex3fv(cubeVertexes[2]);
+	glVertex3fv(cubeVertexes[3]);
+	glVertex3fv(cubeVertexes[1]);
+	glEnd();
+	glBegin(GL_POLYGON);
+	glColor3f(1.0, 0.0, 0.0);
+	glNormal3f(0, -1, 0);
+	glVertex3fv(cubeVertexes[4]);
+	glVertex3fv(cubeVertexes[5]);
+	glVertex3fv(cubeVertexes[7]);
+	glVertex3fv(cubeVertexes[6]);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3f(0.0, 1.0, 0.0);
+	glNormal3f(0, 0, 1);
+	glVertex3fv(cubeVertexes[2]);
+	glVertex3fv(cubeVertexes[6]);
+	glVertex3fv(cubeVertexes[7]);
+	glVertex3fv(cubeVertexes[3]);
+	glEnd();
+	glBegin(GL_POLYGON);
+	glColor3f(0.0, 0.0, 1.0);
+	glNormal3f(0, 0, -1);
+	glVertex3fv(cubeVertexes[0]);
+	glVertex3fv(cubeVertexes[1]);
+	glVertex3fv(cubeVertexes[5]);
+	glVertex3fv(cubeVertexes[4]);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3f(1.0, 1.0, 0.0);
+	glNormal3f(-1, 0, 0);
+	glVertex3fv(cubeVertexes[0]);
+	glVertex3fv(cubeVertexes[4]);
+	glVertex3fv(cubeVertexes[6]);
+	glVertex3fv(cubeVertexes[2]);
+	glEnd();
+	glBegin(GL_POLYGON);
+	glColor3f(0.0, 1.0, 1.0);
+	glNormal3f(1, 0, 0);
+	glVertex3fv(cubeVertexes[1]);
+	glVertex3fv(cubeVertexes[3]);
+	glVertex3fv(cubeVertexes[7]);
+	glVertex3fv(cubeVertexes[5]);
+	glEnd();
+}
